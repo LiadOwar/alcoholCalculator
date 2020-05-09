09d66b554e4fdbf2c42d0512a71596091e1d0a32
LiadOwaR <liadowar@gmail.com>
2020-05-09 13:38:43 +0300

    configure gauge continue - separate sections and add ticks[]

--- app/src/main/java/liad/com/alcoholcalc/SessionActivity.java
+++ app/src/main/java/liad/com/alcoholcalc/SessionActivity.java
@@ -1,10 +1,12 @@
 package liad.com.alcoholcalc;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.github.anastr.speedviewlib.SpeedView;
+import com.github.anastr.speedviewlib.components.Section;
 import com.google.common.collect.Lists;
 
 import java.util.List;
@@ -22,9 +24,27 @@ public class SessionActivity extends AppCompatActivity {
         imageViews = Lists.newArrayList();
         configBeverageImageViews();
         SpeedView speedometer = (SpeedView)findViewById(R.id.speedView);
+
+
         speedometer.setSpeedAt(1f);
         speedometer.setMinMaxSpeed(0.0f, 50f);
-        speedometer.setUnit("AlcoScore");
+        speedometer.setUnit("Score");
+        speedometer.setWithTremble(true);
+        speedometer.clearSections();
+        speedometer.makeSections(2, Color.GREEN, Section.Style.ROUND);
+        Section greenSection = speedometer.getSections().get(0);
+        Section yellowSection = speedometer.getSections().get(1);
+
+        greenSection.setColor(Color.GREEN);
+        greenSection.setStartOffset(0);
+        greenSection.setEndOffset(0.3f);
+
+        yellowSection.setColor(Color.YELLOW);
+        yellowSection.setStartOffset(0.31f);
+        yellowSection.setEndOffset(1);
+        speedometer.setTickNumber(7);
+
+
     }
 
     private void configBeverageImageViews() {
